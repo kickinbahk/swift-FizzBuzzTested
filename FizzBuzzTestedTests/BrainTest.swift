e6ab10544c3ed7094f08e072c3ebbb002a44079b
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-11-11 08:04:36 -0800

    add new test for divisible by 5

--- FizzBuzzTestedTests/BrainTest.swift
+++ FizzBuzzTestedTests/BrainTest.swift
@@ -12,24 +12,42 @@ import XCTest
 class BrainTest: XCTestCase {
     
   override func setUp() {
-      super.setUp()
-      // Put setup code here. This method is called before the invocation of each test method in the class.
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
   }
   
   override func tearDown() {
-      // Put teardown code here. This method is called after the invocation of each test method in the class.
-      super.tearDown()
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
   }
   
   func testIsDivisibleByThree() {
-    let brain = Brain()
+   let brain = Brain()
     let result = brain.isDivisibleByThree(3)
     XCTAssertEqual(result, true)
   }
   
   func testIsOneDivisibleByThree() {
-    let brain = Brain()
+   let brain = Brain()
     let result = brain.isDivisibleByThree(1)
     XCTAssertEqual(result, false)
   }
+  
+  func testIsDivisibleByFive() {
+    let brain = Brain()
+    let result = brain.isDivisibleByFive(5)
+    XCTAssertEqual(result, true)
+  }
+
+
 }
+
+
+
+
+
+
+
+
+
+
