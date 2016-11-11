e3eb77fb3674606907cba1ba862c40378537937b
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-11-11 11:25:39 -0800

    add game tests file and game class

---
+++ FizzBuzzTested/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  FizzBuzzTested
+//
+//  Created by Josiah Mory on 11/11/16.
+//  Copyright © 2016 kickinbahk Productions. All rights reserved.
+//
+
+import Foundation
+
+class Game: NSObject {
+  
+}
---
+++ FizzBuzzTestedTests/GameTest.swift
@@ -0,0 +1,27 @@
+//
+//  GameTest.swift
+//  FizzBuzzTested
+//
+//  Created by Josiah Mory on 11/11/16.
+//  Copyright © 2016 kickinbahk Productions. All rights reserved.
+//
+
+import XCTest
+
+class GameTest: XCTestCase {
+  
+  let game = Game()
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  
+    
+}
