5c7c3a1382d6c41c814222651f51dfbc6c99aad5
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-11-11 07:42:29 -0800

    add first test, add new class

--- FizzBuzzTestedTests/BrainTest.swift
+++ FizzBuzzTestedTests/BrainTest.swift
@@ -10,14 +10,19 @@ import XCTest
 
 class BrainTest: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-      
+  override func setUp() {
+      super.setUp()
+      // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+      // Put teardown code here. This method is called after the invocation of each test method in the class.
+      super.tearDown()
+  }
+  
+  func testIsDivisibleByThree() {
+    let brain = Brain()
+    let result = brain.isDivisibleByThree(3)
+    XCTAssertEqual(result, true)
+  }
 }
