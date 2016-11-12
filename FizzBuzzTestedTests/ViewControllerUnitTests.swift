28a352585f5423291d465505586a754f1440f769
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-11-11 16:30:03 -0800

    add UI unit test file

---
+++ FizzBuzzTestedTests/ViewControllerUnitTests.swift
@@ -0,0 +1,46 @@
+//
+//  ViewControllerUnitTests.swift
+//  FizzBuzzTested
+//
+//  Created by Josiah Mory on 11/11/16.
+//  Copyright © 2016 kickinbahk Productions. All rights reserved.
+//
+
+import XCTest
+@testable import FizzBuzzTested
+
+class ViewControllerUnitTests: XCTestCase {
+  
+  var viewController: ViewController!
+
+  override func setUp() {
+    super.setUp()
+    
+    let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+    viewController = storyboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+    UIApplication.shared.keyWindow!.rootViewController = viewController
+    
+    let _ = viewController.view
+  }
+
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+
+  func test() {
+    XCTAssertTrue(true)
+  }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
